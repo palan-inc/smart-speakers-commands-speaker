342f7a46c6d7691146766e42c91be38c51a437c2
eishi <saito@eishis.com>
2018-04-29 18:21:09 +0900

    add const files

---
+++ src/const/alexa.js
@@ -0,0 +1,10 @@
+export const Alexa = {
+    pre_commands: 'アレクサ、',
+    commands: [
+      {
+        title: '音楽かけて',
+        pronunciation: 'おんがくかけて',
+        description: 'ランダムで音楽をかけてくれるコマンドです。',
+      },
+    ]
+  }
---
+++ src/const/google.js
@@ -0,0 +1,10 @@
+export const Google = {
+  pre_commands: 'OK Google、',
+  commands: [
+    {
+      title: '音楽かけて',
+      pronunciation: 'おんがくかけて',
+      description: 'ランダムで音楽をかけてくれるコマンドです。',
+    },
+  ]
+}
